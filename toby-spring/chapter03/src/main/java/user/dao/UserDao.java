7ba310b2e9baa85256c9d6a42b4d7b1d7725cabb
성준영 <wnsdud1861@gmail.com>
2017-07-18 23:13:37 +0900

    토비의 스프링 3.1 / Chapter02 테스트 - 3 (끝)

--- toby-spring/chapter03/src/main/java/user/dao/UserDao.java
+++ toby-spring/chapter03/src/main/java/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package user.dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import user.domain.User;
 
 import javax.sql.DataSource;
@@ -10,93 +11,54 @@ import java.sql.*;
  * Created by junyoung on 2017. 7. 8..
  */
 public class UserDao {
-	private DataSource dataSource;
-	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
-		this.jdbcContext = new JdbcContext();
-		this.jdbcContext.setDataSource(dataSource);
-		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-			@Override
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
+	private RowMapper<User> userMapper = new RowMapper<User>(){
 
-				return ps;
-			}
-		});
-	}
-
-	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
-
-		PreparedStatement ps = c.prepareStatement("SELECT * FROM users where id = ?");
-		ps.setString(1, id);
-
-		ResultSet rs = ps.executeQuery();
-
-		User user = null;
-		if (rs.next()) {
-			user = new User();
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
 			user.setId(rs.getString("id"));
 			user.setName(rs.getString("name"));
 			user.setPassword(rs.getString("password"));
-
+			return user;
 		}
+	};
 
-		rs.close();
-		ps.close();
-		c.close();
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		this.jdbcTemplate.update("INSERT INTO users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+	}
 
-		if (user == null) throw new EmptyResultDataAccessException(1);
-
-		return user;
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		return this.jdbcTemplate.queryForObject("SELECT * FROM users WHERE id = ?", new Object[]{id},this.userMapper);
 	}
 
 
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update("DELETE FROM users");
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//			@Override
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				return con.prepareStatement("SELECT count(*) FROM users");
+//			}
+//		}, new ResultSetExtractor<Integer>() {
+//			@Override
+//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//				rs.next();
+//				return rs.getInt(1);
+//			}
+//		});
+		return this.jdbcTemplate.queryForInt("SELECT count(*) FROM users");
 	}
 
+	public java.util.List<User> getAll() {
+		return this.jdbcTemplate.query("SELECT * FROM users ORDER BY id",this.userMapper);
+	}
 }
--- toby-spring/chapter03/src/main/java/user/test/UserDaoTest.java
+++ toby-spring/chapter03/src/main/java/user/test/UserDaoTest.java
@@ -1,5 +1,6 @@
 package user.test;
 
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -89,5 +90,39 @@ public class UserDaoTest {
 		assertThat(dao.getCount(), is(0));
 
 		dao.get("unknown_id");
+	}
+
+	@Test
+	public void getAll() throws SQLException, ClassNotFoundException {
+		dao.deleteAll();
+		dao.add(user1);
+		List<User> users = dao.getAll();
+		assertThat(users.size(), is(1));
+		checkSameUser(user1, users.get(0));
+
+		dao.add(user2);
+		users.add(user2);
+		assertThat(users.size(), is(2));
+		checkSameUser(user2, users.get(1));
+
+		dao.deleteAll();
+		dao.add(user1);
+		dao.add(user2);
+		dao.add(user3);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(3));
+		checkSameUser(user1, users1.get(0));
+		checkSameUser(user2, users1.get(1));
+		checkSameUser(user3, users1.get(2));
+
+		dao.deleteAll();
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(0));
+	}
+
+	private void checkSameUser(User user1, User user2){
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
 }
